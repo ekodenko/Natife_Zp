be293e0ec3122fff5d3a3ac2ca52090ebca86e82
Yevhen <92868360+ekodenko@users.noreply.github.com>
2022-02-18 12:28:31 +0200

    1

--- Natife_Zp/ViewController.swift
+++ Natife_Zp/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var SortbyDate: UIScrollView!
+    @IBOutlet weak var SortbyRating: UIScrollView!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    
     }
 
 
